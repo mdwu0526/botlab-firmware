90bbf5d4351f11d797659829e28f46c32a7b6a6d
Chien Erh Lin <chienerh@umich.edu>
2022-08-26 14:52:57 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 echo "[Setup] Apt Installing required packages..."
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
+echo "[Setup] Set up permission..."
+sudo chown -R pi:pi .git
+sudo chmod -R 775 .git
 echo "[Setup] Updating submodules..."
 cd lib && git submodule update --init
 cd pico-sdk && git submodule update --init
+git checkout master
+git pull
 cd ../../
 echo "[Setup] Done!"
+

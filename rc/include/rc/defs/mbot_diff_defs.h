c52a3b60053a89f0ae610823de20c3e18136f50c
pgaskell <pgaskell@umich.edu>
2022-10-31 09:42:21 -0400

    fixed FRAM memory addresses

--- rc/include/rc/defs/mbot_diff_defs.h
+++ rc/include/rc/defs/mbot_diff_defs.h
@@ -1,11 +1,11 @@
 #include <rc/defs/common_defs.h>
 
 typedef enum mbot_fram_cfg_length_t{
-	WHEEL_CALIBRATION_LEN = 4 * sizeof(float), // 6 floats
+	WHEEL_CALIBRATION_LEN = 8 * sizeof(float), // 8 floats
 	PID_VALUES_LEN = 20 * sizeof(float), // 20 floats
 } mbot_fram_cfg_length_t;
 
 typedef enum mbot_fram_cfg_offset_t{
 	WHEEL_CALIBRATION_ADDR = MPU_FINAL_FRAM_ADDR, // have to start at 102 since thats where the MPU stops
-	PID_VALUES_ADDR = WHEEL_CALIBRATION_ADDR + PID_VALUES_LEN,
+	PID_VALUES_ADDR = WHEEL_CALIBRATION_ADDR + WHEEL_CALIBRATION_LEN,
 } mbot_fram_cfg_offset_t;

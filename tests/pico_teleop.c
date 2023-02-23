05740a7ff07a0d7da4c631aa073c00df3191a08a
Onur Bagoren <onurbagoren3@gmail.com>
2023-02-23 00:44:41 -0500

    Teleop test

---
+++ tests/pico_teleop.c
@@ -0,0 +1,193 @@
+#include <pico/stdlib.h>
+#include <pico/mutex.h>
+#include <pico/multicore.h>
+#include <pico/time.h>
+#include <rc/motor/motor.h>
+#include <rc/encoder/encoder.h>
+#include <rc/motor/motor.h>
+#include <rc/mpu/mpu.h>
+#include <comms/common.h>
+#include <comms/protocol.h>
+#include <comms/listener.h>
+#include <comms/topic_data.h>
+#include <comms/mbot_messages.h>
+
+
+#define LED_PIN 25
+#define MAIN_LOOP_HZ 50.0
+#define MAIN_LOOP_PERIOD (1.0f / MAIN_LOOP_HZ)
+
+timestamp_t received_time = {0};
+// current body frame command
+mbot_motor_command_t current_cmd = {0};
+static i2c_inst_t *i2c;
+static rc_mpu_data_t mpu_data;
+
+uint64_t timestamp_offset = 0;
+uint64_t current_pico_time = 0;
+
+void register_topics(void);
+void mainloop(void);
+float clamp_duty(float duty);
+void timestamp_cb(timestamp_t *received_timestamp)
+{
+    // if we havent set the offset yet
+    if (timestamp_offset == 0)
+    {
+        uint64_t cur_pico_time = to_us_since_boot(get_absolute_time());
+        timestamp_offset = received_timestamp->utime - cur_pico_time;
+    }
+}
+
+void register_topics(void)
+{
+    // timesync topic
+    comms_register_topic(MBOT_TIMESYNC, sizeof(timestamp_t), (Deserialize)&timestamp_t_deserialize, (Serialize)&timestamp_t_serialize, (MsgCb)&timestamp_cb);
+    // motor commands topic
+    comms_register_topic(MBOT_MOTOR_COMMAND, sizeof(mbot_motor_command_t), (Deserialize)&mbot_motor_command_t_deserialize, (Serialize)&mbot_motor_command_t_serialize, NULL);
+}
+
+bool timer_cb(repeating_timer_t *rt)
+{
+    // only run if we've received a timesync message...
+    if (comms_get_topic_data(MBOT_TIMESYNC, &received_time))
+    {
+        uint64_t cur_pico_time = to_us_since_boot(get_absolute_time()) + timestamp_offset;
+        uint64_t latency_time = cur_pico_time - current_pico_time;
+        current_pico_time = cur_pico_time;
+
+        // get the current motor command state (if we have one)
+        if (comms_get_topic_data(MBOT_MOTOR_COMMAND, &current_cmd))
+        {
+            int16_t l_cmd, r_cmd;                 // left and right motor commands
+            float l_duty, r_duty;                 // duty cycle in range [-1, 1]
+
+            if(current_cmd.angular_v == 0)
+            {
+                // if we are not turning, just set the left and right speeds
+                l_duty = current_cmd.trans_v;
+                r_duty = current_cmd.trans_v;
+            }
+            else
+            {
+                // if we are turning, set the left and right speeds
+                l_duty = current_cmd.trans_v - current_cmd.angular_v * 0.15 / 2.0;
+                r_duty = current_cmd.trans_v + current_cmd.angular_v * 0.15 / 2.0;
+            }
+
+            // Clamp duty cycle to [-1, 1]
+            l_duty = clamp_duty(l_duty);
+            r_duty = clamp_duty(r_duty);
+
+            // duty to motor command
+            l_cmd =(int)(l_duty * 0.95 * pow(2, 15));
+            r_cmd =(int)(r_duty * 0.95 * pow(2, 15));
+
+            // set left and right motor command
+            rc_motor_set(1, l_cmd);
+            rc_motor_set(3, r_cmd);
+        }
+        uint64_t fn_run_len = to_us_since_boot(get_absolute_time()) + timestamp_offset - cur_pico_time;
+    }
+
+    return true;
+}
+
+int main()
+{
+    bi_decl(bi_program_description("Firmware for the MBot Robot Control Board"));
+    
+    set_sys_clock_khz(250000, true); // set master clock to 250MHz (if problematic try 125Mhz)
+    stdio_init_all(); // enable USB serial terminal
+    sleep_ms(1500); // quick sleep so we can catch the bootup process in terminal
+    printf("\nMBot Booting Up!\n");
+
+    printf("initializinging motors...\n");
+    rc_motor_init();
+    printf("initializinging encoders...\n");
+    rc_encoder_init();
+
+    // Pins
+    // for the i2c to the IMU
+    const uint sda_pin = 4;
+    const uint scl_pin = 5;
+
+    // Ports
+    i2c = i2c0;
+    // Initialize I2C port at 400 kHz
+    i2c_init(i2c, 400 * 1000);
+    // Initialize I2C pins
+    printf("setting i2c functions...\n");
+    gpio_set_function(sda_pin, GPIO_FUNC_I2C);
+    gpio_set_function(scl_pin, GPIO_FUNC_I2C);
+    printf("setting i2c pullups...\n");
+    gpio_pull_up(sda_pin);
+    gpio_pull_up(scl_pin);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(sda_pin, scl_pin, GPIO_FUNC_I2C));
+
+    printf("setting heartbeat LED GPIOs...\n");
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Initialize the IMU using the Digital Motion Processor
+    printf("initializing DMP...\n");
+    rc_mpu_config_t mpu_config = rc_mpu_default_config();
+    mpu_config.i2c_bus = i2c;
+    mpu_config.dmp_fetch_accel_gyro = 1;
+    mpu_config.enable_magnetometer = 1;
+    mpu_config.read_mag_after_callback = 0;
+    mpu_config.orient = ORIENTATION_Z_UP;
+    mpu_config.dmp_sample_rate = 200;
+    
+    // Calibrate the gyro to eliminate bias, Mbot must be still for this
+    rc_mpu_calibrate_gyro_routine(mpu_config);
+    sleep_ms(500);
+    rc_mpu_initialize_dmp(&mpu_data, mpu_config);
+    gpio_set_irq_enabled_with_callback(rc_MPU_INTERRUPT_GPIO, GPIO_IRQ_EDGE_FALL, true, &rc_dmp_callback);
+    printf("MPU Initialized!\n");
+
+    // create topics and register the serialize/deserialize functions
+    printf("init comms...\r\n");
+    comms_init_protocol();
+    comms_init_topic_data();
+    register_topics();
+
+    // launch the other core's comm loop
+    printf("starting comms on core 1...\r\n");
+    multicore_launch_core1(comms_listener_loop);
+
+    // wait for other core to get rolling
+    sleep_ms(500);
+    int running = 1;
+
+    // run the main loop as a timed interrupt
+    printf("starting the timed interrupt...\r\n");
+    repeating_timer_t loop_timer;
+    add_repeating_timer_ms(MAIN_LOOP_PERIOD * 1000, timer_cb, NULL, &loop_timer); // 1000x to convert to ms
+
+    printf("Done Booting Up!\n\n");
+
+    while (running)
+    {
+        int asdf;
+    }
+}
+
+/**
+ * @brief Clamp duty cycle between -1 and 1. If not applied, robot drives in reverse only
+ *
+ * @param duty
+ */
+float clamp_duty(float duty)
+{
+    if (duty > 1.0)
+    {
+        return 1.0;
+    }
+    else if (duty < -1.0)
+    {
+        return -1.0;
+    }
+    return duty;
+}

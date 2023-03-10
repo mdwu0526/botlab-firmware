03e44003ee1693eb5992b556db0d3a9eb59c81c0
mdwu <mdwu@umich.edu>
2023-03-10 11:48:05 -0500

    Testing gitignore

--- rc/include/rc/defs/mbot_diff_defs.h
+++ rc/include/rc/defs/mbot_diff_defs.h
@@ -5,7 +5,7 @@
 
 #define LEFT_MOTOR_CHANNEL      1
 #define RIGHT_MOTOR_CHANNEL     3
-#define WHEEL_RADIUS            0.04      // change your wheel radius to your specific measurements
+#define WHEEL_RADIUS            TEST     // change your wheel radius to your specific measurements
 #define WHEEL_BASE              0.165      // change your wheelbase to your specific measurements
 
 #define M1_SLOPE				-0.00580285 		  // define your mbot-specific motor 1 slope (duty cycle / wheel speed)

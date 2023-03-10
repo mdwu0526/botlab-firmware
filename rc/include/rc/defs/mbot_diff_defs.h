abf15a4bc344da5108e36e4949a3af43ab0d1a59
mdwu <mdwu@umich.edu>
2023-03-10 11:46:01 -0500

    Diff Defs Test

--- rc/include/rc/defs/mbot_diff_defs.h
+++ rc/include/rc/defs/mbot_diff_defs.h
@@ -10,8 +10,8 @@
 
 #define M1_SLOPE				-0.00580285 		  // define your mbot-specific motor 1 slope (duty cycle / wheel speed)
 #define M1_INT					0.11208425 		  // define your mbot-specific motor 1 intercept (duty cycle to get motor moving)
-#define M3_SLOPE 				0.00576415 		  // define your mbot-specific motor 3 slope (duty cycle / wheel speed)
-#define M3_INT 					0.1011394 		  // define your mbot-specific motor 3 intercept (duty cycle to get motor moving)
+#define M3_SLOPE 				0.006224 		  // define your mbot-specific motor 3 slope (duty cycle / wheel speed)
+#define M3_INT 					0.00576415 		  // define your mbot-specific motor 3 intercept (duty cycle to get motor moving)
 
 typedef enum mbot_fram_cfg_length_t{
 	WHEEL_CALIBRATION_LEN = 8 * sizeof(float), // 8 floats
